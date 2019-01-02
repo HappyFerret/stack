d8d55361af5b301f29129c3943f257514f6201ab
Antoine Savignac <antoine.savignac@mylocalheroes.com>
2019-01-02 13:48:45 +0000

    Revert "Remove extra volume stack"
    This reverts commit e89b1b67ff2335078f45b0b7904f3039e0965534.

--- ecs-cluster/main.tf
+++ ecs-cluster/main.tf
@@ -191,6 +191,13 @@ resource "aws_launch_configuration" "main" {
     volume_size = "${var.root_volume_size}"
   }
 
+  # docker
+  ebs_block_device {
+    device_name = "/dev/xvdcz"
+    volume_type = "gp2"
+    volume_size = "${var.docker_volume_size}"
+  }
+
   lifecycle {
     create_before_destroy = true
   }
